738a3d6b2081022d9f8ba5e568c55a09ac1e0f0f
Jun Tian <tianjun.cpp@gmail.com>
2020-01-01 19:17:08 +0800

    remove lib extension

--- open_spiel/julia/deps/deps.jl
+++ open_spiel/julia/deps/deps.jl
@@ -1,7 +1,7 @@
 const LIB_OPEN_SPIEL = begin
-    lib_path = "$(dirname(@__FILE__))/../../../build/julia/libspieljl.so"
+    lib_path = "$(dirname(@__FILE__))/../../../build/julia/libspieljl"
     if !isfile(lib_path)
-        @error "libspieljl.so not found. Please check the build status!"
+        @error "libspieljl not found. Please check the build status!"
     end
     lib_path
 end
